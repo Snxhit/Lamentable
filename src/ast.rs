c9a208d6dde409a35c872e770002bffc8c6ed4f5
Snxhit_ <74500652+Snxhit@users.noreply.github.com>
2025-07-02 08:34:08 +0530

    Delete src directory

--- src/ast.rs
+++
@@ -1,8 +0,0 @@
-#[derive(Debug)]
-pub enum ASTNode
-{
-    ShoutStatement { val: String, ident: bool},
-    LetStatement { datatype: String, name: String, val: String }, // val is converted to f64 later on.
-    ConstStatement { datatype: String, name: String, val: String },
-    IfStatement { tree: Vec<ASTNode> },
-}
