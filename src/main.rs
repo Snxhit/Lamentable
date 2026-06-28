76a2ce59edc52ba0b955746f1a7873182cfb6fd8
Snxhit <amitdgames@gmail.com>
2026-06-28 13:41:10 +0530

    use string instead of file

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 use std::fs;
 
 fn main() {
-    let source = fs::read_to_string("main.lm")
-        .expect("Failed to read Lamentable source file!");
+    //let source = fs::read_to_string("main.lm").expect("Failed to read Lamentable source file!");
+    let source = "Hello World";
     println!("{}", source);
 }
