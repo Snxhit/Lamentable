df6d2dba502c337930c83b07b7940f4e507f2057
Snxhit <amitdgames@gmail.com>
2026-06-28 15:19:30 +0530

    add lexer tokenizer

--- src/lexer.rs
+++ src/lexer.rs
@@ -1,5 +1,5 @@
-use crate::Token::token;
-use std::iter::peekable;
+use crate::token::Token;
+use std::iter::Peekable;
 use std::str::Chars;
 
 pub struct Lexer<'a> {
@@ -11,5 +11,26 @@ impl<'a> Lexer<'a> {
         Lexer {
             chars: source.chars().peekable(),
         }
+    }
+
+    pub fn tokenize(&mut self) -> Vec<Token> {
+        let mut tokens = Vec::new();
+
+        while let Some(ch) = self.chars.next() {
+            match ch {
+                ' ' | '\n' => continue,
+                '+' => tokens.push(Token::Plus),
+
+                '0'..='9' => {
+                    let val = ch.to_digit(10).unwrap() as i64;
+                    tokens.push(Token::Number(val));
+                }
+
+                _ => panic!("Lexing Error: Unknown character {}", ch),
+            }
+        }
+
+        tokens.push(Token::EOF);
+        tokens
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,18 @@
 use std::fs;
+mod lexer;
+mod token;
+
+use lexer::Lexer;
 
 fn main() {
     //let source = fs::read_to_string("main.lm").expect("Failed to read Lamentable source file!");
-    let source = "Hello World";
+    let source = "++ 12 + 24+1";
     println!("{}", source);
+
+    let mut lexer = Lexer::new(&source);
+    let tokens = lexer.tokenize();
+
+    for t in &tokens {
+        println!("{:?}", t);
+    }
 }
